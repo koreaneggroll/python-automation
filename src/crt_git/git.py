136687a884e375f94e0f9a0ef77b540494a92311
Arin <aringrigoras@gmail.com>
2021-08-10 22:37:59 +0300

    tried to create a special directory but failed

--- src/crt_git/git.py
+++ src/crt_git/git.py
@@ -1,12 +1,47 @@
 import os
 
 
+
+
+def setup():
+    git_username = input("what is your github username? ")
+    git_pass = input("what is your github password? ")
+
+
+    directory = "crt_git"
+
+    parent_directory = "~/automation/"
+
+
+    path = os.path.join(parent_directory, directory)
+
+    os.mkdir(path)
+
+    print(f"created {directory} in path ${path}")
+
+
+
+    
+
+
+
 def open_github():
     os.system("firefox https://github.com/new")
 
 
+
+
 def main():
+
+    if not os.path.isfile("~/automation/crt_git/cred.txt"):
+        setup()
+    else:
+        pass
+
     open_github()
+
+    git_name = input("what is the name of the repo? ")
+
     
 
 
