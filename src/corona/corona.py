d9bfc318c2426ccd5dfbf7c91a6b9f65bea7370e
Arin <aringrigoras@gmail.com>
2021-08-16 15:43:50 +0300

    made a function for each file

--- src/corona/corona.py
+++ src/corona/corona.py
@@ -1,12 +1,21 @@
 from covid import *
 
-country = input("Where do you live? ")
 
-print()
+def corona():
+    country = input("Where do you live? ")
 
-covid = Covid()
-print(covid.get_status_by_country_name(country))
+    print()
 
-print()
+    covid = Covid()
+    print(covid.get_status_by_country_name(country))
 
-print(f"This is the data for {country}")
+    print()
+
+    print(f"This is the data for {country}")
+
+
+def main():
+    corona()
+
+
+main()
--- src/wallpaper/wallpaper.py
+++ src/wallpaper/wallpaper.py
@@ -3,21 +3,31 @@ import time
 import os
 
 
+def wallpaper():
+    time_sleep = 3600  # one hour in seconds
 
-while True:
-    time_sleep = 3600 #one hour in seconds
-    now = datetime.now()
+    while True:
+        now = datetime.now()
 
-    current_time = now.strftime("%H")
-    int_current_time = int(current_time)
+        current_time = now.strftime("%H")
+        int_current_time = int(current_time)
 
-    if int_current_time < 12:
-        print("Switching to ")
-        os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/arin/python-automation-wallpapers/light-theme.jpg")
+        if int_current_time < 12:
+            print("Switching to ")
+            os.system(
+                "/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/arin/python-automation-wallpapers/light-theme.jpg")
 
 
-    elif int_current_time > 12:
-        print("Switching to dark mode")
-        os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/arin/python-automation-wallpapers/dark-theme.jpg")
+        elif int_current_time > 12:
+            print("Switching to dark mode")
+            os.system(
+                "/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/arin/python-automation-wallpapers/dark-theme.jpg")
 
-    time.sleep(time_sleep)
+        time.sleep(time_sleep)
+
+
+def main():
+    wallpaper()
+
+
+main()
