b9f1618a8283174e1e3caf5e54968f31b7652747
Arin <aringrigoras@gmail.com>
2021-08-16 17:34:05 +0300

    its cool but i'll see what i can do later

--- src/fiverr/fiverr.py
+++ src/fiverr/fiverr.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x1 = [0, 1, 2, 3, 4, 5, 6]
-y1 = [0, 7, 4, 7, 2, 5, 8]
+x1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+y1 = [0, 12, 4, 2, 5, 1, 5, 17, 4]
 
 
 plt.plot(x1, y1, label = "impressions")
 
 x2 = [0, 1, 2, 3, 4, 5, 6]
-y2 = [0, 0, 3, 0, 0, 0, 1]
+y2 = [0, 2, 0, 0, 0, 1, 0 ]
 
 plt.plot(x2, y2, label = "clicks")
 
