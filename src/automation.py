83dfc04d7405eedce2ea18e51cff7daa1c8f1c78
koreaneggroll <aringrigoras@gmail.com>
2021-08-16 21:21:46 +0300

    automation.py
    it incorporates every single file created so far.

---
+++ src/automation.py
@@ -0,0 +1,18 @@
+import corona, internet, wallpaper
+
+
+def main():
+    #wallpaper.py
+    wallpaper.wallpaper()
+    
+    #internet.py
+    internet.download_speed()
+    internet.upload_speed()
+    internet.ping()
+
+
+    #corona.py
+    corona.corona()
+
+
+main()
