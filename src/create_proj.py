fbeb4d2f5cbe9c8c40a82f232856caa914a72c72
koreaneggroll <aringrigoras@gmail.com>
2021-08-17 23:50:00 +0300

    Create create_proj.py

---
+++ src/create_proj.py
@@ -0,0 +1,42 @@
+import os
+
+
+c_proj = "C"
+cpp_proj = "C++"
+py_proj = "python"
+web_proj = "web"
+
+
+def evaluate(x):
+    if x == c_proj:
+        create_c_proj()
+    
+    elif x == cpp_proj:
+        create_cpp_proj()
+
+    elif x == py_proj:
+        create_py_proj()
+
+    elif x == web_proj:
+        create_web_proj()
+
+
+
+def create_c_proj():
+    
+
+def create_cpp_proj():
+
+
+
+def create_py_proj():
+
+
+def create_web_proj():
+
+
+
+def main():
+    print("Hello world")
+
+main()
