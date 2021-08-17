4c434f7a133f978ae59e7d2ca72613c5e264e6c5
koreaneggroll <aringrigoras@gmail.com>
2021-08-17 23:57:27 +0300

    creating the project directory

--- src/create_proj.py
+++ src/create_proj.py
@@ -9,34 +9,83 @@ web_proj = "web"
 
 def evaluate(x):
     if x == c_proj:
-        create_c_proj()
+        dirName = input("name of the project? ")
+
+        create_c_proj(dirName)
     
+
     elif x == cpp_proj:
-        create_cpp_proj()
+        dirName = input("name of the project? ")
+
+
+        create_cpp_proj(dirName)
+
 
     elif x == py_proj:
-        create_py_proj()
+        dirName = input("name of the project? ")
+
+        create_py_proj(dirName)
+
 
     elif x == web_proj:
-        create_web_proj()
+        dirName = input("name of the project? ")
+
+        create_web_proj(dirName)
+
+
+    else:
+        print(f"No option '{x}'")
 
 
 
-def create_c_proj():
-    
 
-def create_cpp_proj():
-
-
-
-def create_py_proj():
+def create_c_proj(dirName):
+    try:
+        os.mkdir(dirName)
+        print(f"Directory {dirName} created")
 
 
-def create_web_proj():
+    except FileExistsError:
+        print(f"Directory {dirName} already exists")
+
+
+
+def create_cpp_proj(dirName):
+    try:
+        os.mkdir(dirName)
+        print(f"Directory {dirName} created")
+
+        
+    except FileExistsError:
+        print(f"Directory {dirName} already exists")
+
+
+
+def create_py_proj(dirName):
+    try:
+        os.mkdir(dirName)
+        print(f"Directory {dirName} created")
+
+
+    except FileExistsError:
+        print(f"Directory {dirName} already exists")
+
+
+
+def create_web_proj(dirName):
+    try:
+        os.mkdir(dirName)
+        print(f"Directory {dirName} created")
+
+
+    except FileExistsError:
+        print(f"Directory {dirName} already exists")
 
 
 
 def main():
-    print("Hello world")
+    project_language = input("What's your project's language of choice? ")
+
+    evaluate(project_language)
 
 main()
